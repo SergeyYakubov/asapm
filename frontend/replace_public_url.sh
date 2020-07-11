90e1c5c01e4b22273eb9e0a1f70041c01d275970
Sergey Yakubov <sergey.yakubov@desy.de>
2020-07-11 19:13:21 +0200

    fix sed

--- frontend/replace_public_url.sh
+++ frontend/replace_public_url.sh
@@ -1,1 +1,2 @@
-find /usr/share/nginx/html -type f -exec sed -i "s/__PLACEHOLDER_PUBLIC_URL__/$PUBLIC_URL/g" {} + -name "*.css" -o -name "*.html" -o -name "*.js"
+
+find /usr/share/nginx/html -type f -exec sed -i "s{__PLACEHOLDER_PUBLIC_URL__{$PUBLIC_URL{g" {} + -name "*.css" -o -name "*.html" -o -name "*.js"
