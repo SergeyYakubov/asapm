7dcb4bec60a1129fdfb0fd8017b67f3cc2d29292
Sergey Yakubov <sergey.yakubov@desy.de>
2020-08-22 19:44:59 +0200

    cleanup

--- api_server/src/server/config.go
+++ api_server/src/server/config.go
@@ -3,34 +3,30 @@ package server
 import (
 	log "asapm/common/logger"
 	"asapm/common/utils"
-	"fmt"
 )
 
 type serverConfig struct {
-	DbEndpoint string `json:"dbEndpoint"`
-	LogLevel string `json:"logLevel"`
-	Port string `json:"port"`
-	BasePath string `json:"basePath"`
+	DbEndpoint    string `json:"dbEndpoint"`
+	LogLevel      string `json:"logLevel"`
+	Port          string `json:"port"`
+	BasePath      string `json:"basePath"`
 	PublicKeyPath string `json:"publicKeyPath"`
-	publicKey string
+	publicKey     string
 }
 
 var Config serverConfig
-
 
 func ReadConfig(fname string) (log.Level, error) {
 	if err := utils.ReadJsonFromFile(fname, &Config); err != nil {
 		return log.FatalLevel, err
 	}
 
-	publicKey,err := utils.ReadFileAsString(Config.PublicKeyPath)
-	if err!=nil {
+	publicKey, err := utils.ReadFileAsString(Config.PublicKeyPath)
+	if err != nil {
 		return log.FatalLevel, err
 	} else {
 		Config.publicKey = publicKey
-		fmt.Println(publicKey)
 	}
-
 
 	level, err := log.LevelFromString(Config.LogLevel)
 
