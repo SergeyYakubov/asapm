dee64b440accce39b0024bfea8cd6c8e74f4520b
Carsten Patzke <carsten.patzke@desy.de>
2020-10-13 13:56:06 +0200

    Added frontend linting

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:jest/recommended',
+    ],
+    plugins: ['react', '@typescript-eslint', 'jest'],
+    env: {
+        browser: true,
+        es6: true,
+        jest: true,
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        project: './tsconfig.json',
+    },
+    rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'error',
+    }
+}
---
+++ frontend/src/KvObj.d.ts
@@ -0,0 +1,1 @@
+declare type KvObj<ValueType = any> = Record<string, ValueType>;
