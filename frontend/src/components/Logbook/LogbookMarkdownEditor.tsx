13f7a0ef20a44f9abfb3c43f1aaf14a94a61dc2a
Carsten Patzke <carsten.patzke@desy.de>
2020-11-11 18:48:14 +0100

    Last commit before testing draft js

--- frontend/src/components/Logbook/LogbookMarkdownEditor.tsx
+++ frontend/src/components/Logbook/LogbookMarkdownEditor.tsx
@@ -1,5 +1,6 @@
 import * as ProseMirrorMarkdown from "prosemirror-markdown";
-const schema = (ProseMirrorMarkdown as any).schema;
+import * as ProseMirrorSchemaBasic from "prosemirror-schema-basic";
+const baseSchema = (ProseMirrorSchemaBasic as any).schema;
 
 import 'prosemirror-view/style/prosemirror.css';
 import 'prosemirror-example-setup/style/style.css';
@@ -10,8 +11,92 @@ import { EditorState, Plugin, PluginKey } from "prosemirror-state";
 import { EditorView } from "prosemirror-view";
 import React, {forwardRef, useEffect, useImperativeHandle, useRef} from "react";
 import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
+import {Dropdown, MenuItem} from "prosemirror-menu";
+import {
+    addColumnAfter,
+    addColumnBefore,
+    addRowAfter,
+    addRowBefore,
+    deleteColumn,
+    deleteRow,
+    deleteTable, fixTables, goToNextCell, isInTable,
+    mergeCells,
+    setCellAttr,
+    splitCell,
+    tableEditing,
+    tableNodes,
+    toggleHeaderCell,
+    toggleHeaderColumn,
+    toggleHeaderRow
+} from "prosemirror-tables";
+import {DOMParser, Schema, Fragment}  from "prosemirror-model";
+
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const {exampleSetup} = require('prosemirror-example-setup');
+const {exampleSetup, buildMenuItems} = require('prosemirror-example-setup');
+
+const schema = new Schema({
+    nodes: baseSchema.spec.nodes.append(tableNodes({
+        tableGroup: "block",
+        cellContent: "block+",
+        cellAttributes: {
+            background: {
+                default: null,
+                getFromDOM(dom) { return (dom as any).style.backgroundColor || null; },
+                setDOMAttr(value, attrs) { if (value) attrs.style = (attrs.style || "") + `background-color: ${value};`; }
+            }
+        }
+    })),
+    marks: baseSchema.spec.marks
+});
+
+console.log(schema.spec);
+
+const menu = buildMenuItems(schema).fullMenu;
+function item(label: string, cmd: any) {
+    return new MenuItem({label, select: cmd, run: cmd});
+}
+
+const tableMenu = [
+    item("Insert column before", addColumnBefore),
+    item("Insert column after", addColumnAfter),
+    item("Delete column", deleteColumn),
+    item("Insert row before", addRowBefore),
+    item("Insert row after", addRowAfter),
+    item("Delete row", deleteRow),
+    item("Delete table", deleteTable),
+    item("Merge cells", mergeCells),
+    item("Split cell", splitCell),
+    item("Toggle header column", toggleHeaderColumn),
+    item("Toggle header row", toggleHeaderRow),
+    item("Toggle header cells", toggleHeaderCell),
+    item("Make cell green", setCellAttr("background", "#dfd")),
+    item("Make cell not-green", setCellAttr("background", null))
+];
+menu.splice(2, 0, [new Dropdown(tableMenu, {label: "Table"})]);
+menu[1][0].content.push(new MenuItem({
+    label: 'Table',
+    select: (s) => !isInTable(s),
+    run: (state, dispatch, view) => {
+        const tr = state.tr.replaceSelectionWith(
+            state.schema.nodes.table.create(
+                undefined,
+                Fragment.fromArray([
+                    state.schema.nodes.table_row.create(undefined, Fragment.fromArray([
+                        state.schema.nodes.table_cell.createAndFill(),
+                        state.schema.nodes.table_cell.createAndFill()
+                    ]))
+                ])
+            )
+        );
+
+        if (dispatch) {
+            dispatch(tr);
+        }
+
+        return true;
+    }}));
+console.log(menu[1][0].content[1]);
+
 
 const reactPropsKey = new PluginKey("reactProps");
 
@@ -42,13 +127,28 @@ const LogbookMarkdownEditor = forwardRef((props, ref) => {
 
     useImperativeHandle(ref, () => ({
         getRawContent(): string {
+            console.log(ProseMirrorMarkdown.defaultMarkdownSerializer);
+            ProseMirrorMarkdown.defaultMarkdownSerializer.nodes['table'] = function(state, node) {
+                state.ensureNewLine();
+                state.write('abc');
+                state.closeBlock(node);
+            };
+            console.log(view.current.state.schema);
             return ProseMirrorMarkdown.defaultMarkdownSerializer.serialize(view.current.state.doc);
         }
     }));
 
     useEffect(() => { // initial render
-        const state = EditorState.create({schema, plugins: exampleSetup({schema}) });
+        let state = EditorState.create({schema, plugins: [
+            tableEditing(),
+        ].concat(exampleSetup({schema, menuContent: menu}))});
+
+        const fix = fixTables(state);
+        if (fix)
+            state = state.apply(fix.setMeta("addToHistory", false));
+
         view.current = new EditorView(viewHost.current, { state });
+
         return () => view.current.destroy();
     }, []);
 
